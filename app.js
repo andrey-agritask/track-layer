e5bb8c2bb3568e51084e51d54423f4d103f1e2f5
Amir Goren <amir.goren@agritask.com>
2020-10-05 12:04:13 +0300

    track layer - continued

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 /* global document, google */
-import {GoogleMapsOverlay as DeckOverlay} from '@deck.gl/google-maps';
-import {GeoJsonLayer, ArcLayer, LineLayer, PathLayer} from '@deck.gl/layers';
-
-// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
-const AIR_PORTS =
-  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
+import { GoogleMapsOverlay as DeckOverlay } from '@deck.gl/google-maps';
+import { IconLayer, PathLayer } from '@deck.gl/layers';
 
 // Set your Google Maps API key here or via environment variable
 const GOOGLE_MAPS_API_KEY = process.env.GoogleMapsAPIKey; // eslint-disable-line
@@ -23,35 +19,75 @@ function loadScript(url) {
 
 loadScript(GOOGLE_MAPS_API_URL).then(() => {
   const map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 51.47, lng: 0.45},
-    zoom: 5
+    center: {lat: -12.908583, lng: -45.778699},
+    zoom: 16
   });
 
   const rawData = require('./track-m.json');
   const rawDataSegments = rawData.segments;
-  const data = rawDataSegments.map(segment => {
+  const paths = rawDataSegments.map(segment => {
     let path = google.maps.geometry.encoding.decodePath(segment.path);
     path = path.map(o => [o.lng(), o.lat()]);
      return {
        path,
-       color: [255, 0, 0],
      };
   });
 
-  const layer = new PathLayer({
+  const markers = [];
+  rawDataSegments.forEach(segment => {
+    segment.markers.forEach(marker => {
+      markers.push({
+        coordinates: [ marker.position.lng, marker.position.lat ],
+        direction: marker.direction,
+        color: marker.color
+      });
+    });
+  });
+
+  const ICON_MAPPING = {
+    marker: {x: 0, y: 0, width: 24, height: 24, mask: true}
+  };
+
+  const pathLayer = new PathLayer({
     id: 'path-layer',
-    data,
+    data: paths,
     pickable: true,
-    widthScale: 20,
-    widthMinPixels: 20,
+    widthScale: 1,
+    widthMinPixels: 2,
     getPath: d => d.path,
-    getColor: d => d.color,
-    getWidth: d => 20
+    getColor: d => [0, 0, 255],
+    getWidth: d => 5
+  });
+
+  const iconLayer = new IconLayer({
+    id: 'icon-layer',
+    data: markers,
+    pickable: true,
+    iconAtlas: '/sprite.png',
+    iconMapping: ICON_MAPPING,
+    getIcon: d => 'marker',
+
+    sizeScale: 1,
+    getPosition: d => d.coordinates,
+    getSize: d => 24,
+    getColor: d => {
+      switch (d.color) {
+        case 'red':
+          return [255, 0, 0];
+        case 'green':
+          return [0, 128, 0];
+        case 'yellow':
+        default:
+          return [255, 255, 0];
+      }
+    },
+    getAngle: d => Number.isFinite(d.direction) ? d.direction : 0
   });
 
   const overlay = new DeckOverlay({
     layers: [
-      layer
+      pathLayer,
+      iconLayer,
     ]
   });
 
